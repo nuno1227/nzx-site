af07a912204fc9819cde90e1d95efec8696bdf9a
nuno1227 <44648069+nuno1227@users.noreply.github.com>
2024-02-16 15:56:51 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+saldo = 1000
+compras = 100
+saldoatual = saldo - compras
+if saldo > 999:
+    print("Seu saldo foi maior que 1000, portanto você recebeu um bônus.")
+else: 
+    print("Seu saldo foi menor que 1000, portanto você não recebeu um bônus.")
+print("O seu saldo atual é de:", saldoatual)
